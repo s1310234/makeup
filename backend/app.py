95ee3c8bb2172fcfb89648408b494da51b791475
s1310234 <ayu1019ayu@gmail.com>
2025-11-14 14:42:06 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    # allow_origins=["*"],
+    allow_origins=[
+    "https://localhost:5173",
+    "http://localhost:5173",
+    "https://makeup-3jf5.onrender.com",
+    "https://your-vercel-domain.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
